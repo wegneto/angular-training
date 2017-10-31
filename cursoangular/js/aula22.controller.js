0d1a6db548431020234217716f9cbc4aba50e935
Wilson Guimarães <wegneto@gmail.com>
2017-10-31 23:46:00 +0000

    [wip] Aula 22: ui.highlight e ui.mask

---
+++ cursoangular/js/aula22.controller.js
@@ -0,0 +1,7 @@
+var app = angular.module('app', []);
+
+app.controller('Aula22Controller', function ($scope) {
+    console.log("### Starting Aula22Controller ####");
+
+    console.log("### Ending Aula22Controller ####");
+});
