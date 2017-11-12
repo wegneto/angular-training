b81e13bd73c9bf3409eb8a64971aa94edd0a9aaa
Wilson Guimarães <wegneto@gmail.com>
2017-11-12 23:39:06 +0000

    Aula 22: AngularUI: highlight e mask

--- cursoangular/js/aula22.controller.js
+++ cursoangular/js/aula22.controller.js
@@ -1,7 +1,13 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.highlight', 'ui.mask', 'ngSanitize']);
 
 app.controller('Aula22Controller', function ($scope) {
     console.log("### Starting Aula22Controller ####");
+
+    $scope.text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar.';
+
+    $scope.maskcpf = '999.999.999-99';
+
+    $scope.maskphone = '(99) 9999-9999';
 
     console.log("### Ending Aula22Controller ####");
 });
