11977956589731ff5da36d2722594d1b17f0c680
Wilson Guimarães <wegneto@gmail.com>
2017-11-14 10:59:18 +0000

    test

---
+++ cursoangular/js/aula24.controller.js
@@ -0,0 +1,9 @@
+var app = angular.module('app', []);
+
+app.controller('Aula24Controller', function ($scope) {
+    console.log("### Starting Aula24Controller ####");
+
+    
+
+    console.log("### Ending Aula24Controller ####");
+});
