feb9ca0465a1da1d71d6920d9a48d4c15935e471
Wilson Guimarães <wegneto@gmail.com>
2017-11-15 14:27:10 +0000

    Aula 24: AngularUI: google maps

--- cursoangular/js/aula24.controller.js
+++ cursoangular/js/aula24.controller.js
@@ -1,9 +1,23 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['uiGmapgoogle-maps']);
 
-app.controller('Aula24Controller', function ($scope) {
+app.controller('Aula24Controller', function($scope){
     console.log("### Starting Aula24Controller ####");
 
-    
+    $scope.map = {
+        center : {
+            latitude: 45, 
+            longitude: -73
+        }, 
+        zoom: 10 
+    };
+
+    $scope.marker = {
+        id: 0,
+        coords: {
+            latitude: 45,
+            longitude: -73
+        }
+    }
 
     console.log("### Ending Aula24Controller ####");
 });
