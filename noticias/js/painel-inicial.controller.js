60d39db9a2f2e4631b44e1bed757ae606ad84d53
Wilson Guimarães <wegneto@gmail.com>
2017-12-01 16:23:43 +0000

    Aula 38: área de atendimento ao cliente

--- noticias/js/painel-inicial.controller.js
+++ noticias/js/painel-inicial.controller.js
@@ -1,7 +1,77 @@
-var app = angular.module('app', ['ui.mask','angular-loading-bar']);
+var app = angular.module('app', ['ui.mask','angular-loading-bar', 'socket-io']);
 
-app.controller('painelInicialController', function($scope, $http) {
+app.controller('painelInicialController', function($scope, $http, socket) {
     console.log("### painelInicialController begin ###");
+    
+    //chat--
+    $scope.senha = "123456";
+    $scope.chat = false;
+    $scope.chatUsuarios = [];
+    $scope.novaMensagem = '';
+    $scope.usuarioAtivo = 0;
+
+    socket.emit('adminlogin', $scope.senha);
+
+    $scope.chatStatus = function() {
+        socket.emit('setChatStatus', $scope.senha);
+    };
+
+    socket.on('chatstatus', function(data){
+        $scope.chat = data.online;
+    });
+
+    socket.on('usuarioentrou', function(email) {
+        $scope.chatUsuarios.push({usuario: email, mensagens: []});
+
+        if ($scope.chatUsuarios.length == 1) {
+            $scope.usuarioAtivo = 0;
+        }
+    });
+
+    socket.on('novamensagemparaadmin', function(mensagem){
+        var index = $scope.buscaUsuario(mensagem.de);
+        $scope.chatUsuarios[index].mensagens.push({de: mensagem.de, msg: mensagem.msg});
+    });
+
+    $scope.buscaUsuario = function(usuario) {
+        var status = false;
+        var cont = 0;
+        while (cont < $scope.chatUsuarios.length) {
+            if ($scope.chatUsuarios[cont].usuario == usuario) {
+                return cont;
+            }
+            cont++;
+        }
+
+        return false;
+    };
+
+    $scope.enviarMensagem = function() {
+        $scope.chatUsuarios[$scope.usuarioAtivo].mensagens.push({
+            de: 'Admin', msg: $scope.novaMensagem
+        });
+
+        socket.emit('enviarmensagemparausuario', {
+            para: $scope.chatUsuarios[$scope.usuarioAtivo].usuario, 
+            msg: $scope.novaMensagem
+        });
+
+        $scope.novaMensagem = '';
+        $scope.scrollDown();
+    };
+
+    $scope.setUsuarioAtivo = function(index) {
+        $scope.usuarioAtivo = index;
+    };
+
+    $scope.scrollDown = function() {
+        setTimeout(function() {
+            $("#mostra_mensagens").scrollTop(1E10);
+        }, 800);
+    };
+
+    //--chat
+    
     $scope.showCadastro = false;
     $scope.noticia = objNoticia();
     $scope.noticias = {};
--- noticias/painel/painel-inicial.php
+++ noticias/painel/painel-inicial.php
@@ -27,12 +27,49 @@
         </nav>
         <div ng-controller="painelInicialController">
 
+            <div class="container" ng-show="chatUsuarios.length > 0">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h2>Atendimentos</h2>
+                        <div class="col-xs-12">
+                            <div id="mostra_mensagens" class="alert alert-info" style="height: 350px; overflow: scroll; overflow-x:hidden;">
+                                <div class="well well-sm" ng-repeat="msg in chatUsuarios[usuarioAtivo].mensagens">
+                                    <p>De: {{msg.de}}</p>
+                                    <p>{{msg.msg}}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="col-xs-12">
+                        <a href="#" ng-repeat="(key,u) in chatUsuarios" ng-click="setUsuarioAtivo(key)" class="btn btn-primary">
+                            {{u.usuario}}
+                        </a>
+                    </div>
+                    <div class="col-xs-10">
+                        <input type="text" class="form-control" placeholder="mensagem" ng-model="novaMensagem" 
+                            ng-keyup="$event.keyCode == 13 ? enviarMensagem() : null">
+                    </div>
+                    <div class="col-xs-2">
+                        <button class="btn btn-primary btn-block" type="button" ng-click="enviarMensagem()" ng-disabled="novaMensagem == ''">
+                            Enviar
+                        </button>
+                    </div>
+                </div>
+            </div>
+
             <div class="container">
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="well well-sm">
                             <button type="button" class="btn btn-primary" ng-click="abreCadastroNoticia()">
                                 Cadastrar notícia
+                            </button>
+                            <button class="btn btn-success" type="button" ng-show="chat == true" ng-click="chatStatus()">
+                                Chat Online
+                            </button>
+                            <button class="btn btn-danger" type="button" ng-show="chat == false" ng-click="chatStatus()">
+                                Chat Offline
                             </button>
                             <a href="../api/logout" class="btn btn-danger pull-right">Logout</a>
                         </div>
@@ -137,6 +174,8 @@
         <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
         <script src="../js/jquery/jquery.gritter.min.js"></script>
         <script src="../js/angular/angular.min.js"></script>
+        <script src="http://localhost:3000/socket.io/socket.io.js"></script>
+        <script src="../js/angular/ng-socket-io.js"></script>
         <script src="../js/angular/ui-utils.min.js"></script>        
         <script src="../js/angular/loading-bar.min.js"></script>
         <script src="../js/painel-inicial.controller.js"></script>
